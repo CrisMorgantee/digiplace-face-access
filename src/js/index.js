d119d33aa1a8fdbec4174d11f2528af1c78d46de
Cristiano Morgante <cristianomorgante@hotmail.com>
2024-01-05 11:55:56 -0300

    change layout

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,19 @@
+// noinspection SpellCheckingInspection
+
 const invokeApi = async (ip, username = 'admin', password = 'td27fk78') => {
   const url = `http://${ip}/fcgi/do?action=OpenDoor&UserName=${username}&Password=${password}&DoorNum=1`
   
   await fetch(url, {
+    headers: {'Access-Control-Allow-Origin': '*'},
+    credentials: "omit",
+    mode: 'no-cors'
+  })
+}
+
+const callSIP = async (callID) => {
+  const sip = `sip:${callID}@sip.digisonic.com.br`
+  
+  await fetch(sip, {
     headers: {'Access-Control-Allow-Origin': '*'},
     credentials: "omit",
     mode: 'no-cors'
