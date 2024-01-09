73a7aeeafafcb5c2d6b5b3c2222d84427f633707
Cristiano Morgante <cristianomorgante@hotmail.com>
2024-01-09 09:46:24 -0300

    update layout

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,21 @@
 // noinspection SpellCheckingInspection
 
-const invokeApi = async (ip, username = 'admin', password = 'td27fk78') => {
+const invokeApi = async (ip, target = null, username = 'admin', password = 'td27fk78') => {
   const url = `http://${ip}/fcgi/do?action=OpenDoor&UserName=${username}&Password=${password}&DoorNum=1`
+  const unlockIcon = document.querySelector(`#unlock-icon-${target}`)
+  const lockIcon = document.querySelector(`#lock-icon-${target}`)
+  
+  lockIcon.classList.add('hidden')
+  unlockIcon.classList.remove('hidden')
   
   await fetch(url, {
     headers: {'Access-Control-Allow-Origin': '*'},
     credentials: "omit",
     mode: 'no-cors'
   })
-}
-
-const callSIP = async (callID) => {
-  const sip = `sip:${callID}@sip.digisonic.com.br`
   
-  await fetch(sip, {
-    headers: {'Access-Control-Allow-Origin': '*'},
-    credentials: "omit",
-    mode: 'no-cors'
-  })
+  setTimeout(() => {
+    lockIcon.classList.remove('hidden')
+    unlockIcon.classList.add('hidden')
+  }, 6500)
 }
